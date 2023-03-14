b59a30ad33b38072a7aae8edbcdaeae8e93a8dfe
napoole <napoole@protonmail.com>
2023-03-14 15:40:39 -0400

    Integrate drop down menu with scriptjs

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-const onToggleMenu=()
+const onToggleMenu = (e) => {
+    const navLinks = document.querySelector('.nav-links');
+    e.name = e.name === 'menu' ? 'close': 'menu';
+    navLinks.classList.toggle('top-[9%]');
+
+}
