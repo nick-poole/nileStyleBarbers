b01207e6952e17b6201ee813bb8b8df23fca3a4c
napoole <napoole@protonmail.com>
2023-03-15 15:33:17 -0400

    Reformat out of indexhtml into servicehtml for index file switch

--- js/script.js
+++ js/script.js
@@ -2,5 +2,4 @@ const onToggleMenu = (e) => {
     const navLinks = document.querySelector('.nav-links');
     e.name = e.name === 'menu' ? 'close': 'menu';
     navLinks.classList.toggle('top-[9%]');
-
 }
